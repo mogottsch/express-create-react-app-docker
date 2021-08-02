4cd0b5688e1dbc0aaafdee989fd5dabbfa976299
Moritz <gottschling.moritz@gmail.com>
2021-08-02 16:40:14 +0200

    Mount and load .env

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 process.env['NODE_CONFIG_DIR'] = __dirname + '/configs';
 
-import 'dotenv/config';
+import dotenv from 'dotenv';
 import App from '@/app';
 import AuthRoute from '@routes/auth.route';
 import IndexRoute from '@routes/index.route';
@@ -8,6 +8,7 @@ import UsersRoute from '@routes/users.route';
 import validateEnv from '@utils/validateEnv';
 
 validateEnv();
+dotenv.config();
 
 const app = new App();
 app.init([new IndexRoute(), new UsersRoute(), new AuthRoute()]);
